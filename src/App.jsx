f661c140a5913fe44d6882f92ec96b9296121821
DESKTOP-NGJO7RE\arnau <arnau.gallego@mataro.epiaedu.cat>
2024-02-05 18:02:27 +0100

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,20 @@ const App = () => {
 
   return (
     <div>
-      < Header course={course}/>
-      < Content exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} part1={part1} part2={part2} part3={part3} />
-      < Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course={course} />
+      <Content
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+        part1={part1}
+        part2={part2}
+        part3={part3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   );
 };
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,3 @@
 export const Header = ({ course }) => {
-    return( 
-    <h1>{course}</h1>
-    );
+  return <h1>{course}</h1>;
 };
