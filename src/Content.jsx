700991c653d350a906d15178b7848cb3232dfd09
DESKTOP-NGJO7RE\arnau <arnau.gallego@mataro.epiaedu.cat>
2024-02-05 18:16:38 +0100

    commit

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,5 +1,7 @@
-import { Part } from "./Part.jsx"
-export const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+import PropTypes from 'prop-types';
+import { Part } from "./Part.jsx";
+
+const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
   return (
     <div>
       <Part part={part1} exercise={exercises1} />
@@ -8,3 +10,14 @@ export const Content = ({ part1, exercises1, part2, exercises2, part3, exercises
     </div>
   );
 }
+
+Content.propTypes = {
+  part1: PropTypes.string.isRequired,
+  exercises1: PropTypes.number.isRequired,
+  part2: PropTypes.string.isRequired,
+  exercises2: PropTypes.number.isRequired,
+  part3: PropTypes.string.isRequired,
+  exercises3: PropTypes.number.isRequired,
+};
+
+export default Content;
